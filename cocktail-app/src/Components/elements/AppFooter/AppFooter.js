3ee38f948b828581544f46ab220a8d9e9cc1206a
Naomi <nhporti2@gmail.com>
2021-07-07 00:02:12 -0500

    Add files via upload

---
+++ cocktail-app/src/Components/elements/AppFooter/AppFooter.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+
+const appFooterStyles = makeStyles({
+  root: {
+    paddingTop: 24,
+    paddingBottom: 24,
+    textAlign: 'center',
+  },
+  a: {
+    color: '#ff5722',
+    '&:hover, &:focus': {
+      color: '#e64a19',
+    }
+  },
+});
+
+export const AppFooter = () => {
+  const classes = appFooterStyles();
+
+  return(
+    <Container maxWidth="lg" className={classes.root}>
+      <p>For my final CT capstone project, I have created a fluid website using Javascript, React and <a href="https://www.thecocktaildb.com" target="_blank" rel="noopener noreferrer" className={classes.a}>TheCocktialDB</a> API to to generate cocktail recipes through random search. <br/>Enjoy! 🍹🥂<br/>The full code and repository can also be viewed on my <a href="https://github.com/nhporti2/cocktail_app_final" target="_blank" rel="noopener noreferrer" className={classes.a}>GitHub</a>.</p>
+    </Container>
+  );
+}

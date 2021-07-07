32f08ffe33dd49eb4e8f11aa2dfe2cd1cc98e0ca
Naomi <nhporti2@gmail.com>
2021-07-07 11:32:52 -0500

    Update AppFooter.js

--- cocktail-app/src/Components/elements/AppFooter/AppFooter.js
+++ cocktail-app/src/Components/elements/AppFooter/AppFooter.js
@@ -21,7 +21,7 @@ export const AppFooter = () => {
 
   return(
     <Container maxWidth="lg" className={classes.root}>
-      <p>For my final CT capstone project, I have created a fluid website using Javascript, React and <a href="https://www.thecocktaildb.com" target="_blank" rel="noopener noreferrer" className={classes.a}>TheCocktialDB</a> API to to generate cocktail recipes through random search. <br/>Enjoy! 🍹🥂<br/>The full code and repository can also be viewed on my <a href="https://github.com/nhporti2/capstone_project" target="_blank" rel="noopener noreferrer" className={classes.a}>GitHub</a>.</p>
+      <p>For my final CT capstone project, I have created a fluid website using Javascript, React and <a href="https://www.thecocktaildb.com" target="_blank" rel="noopener noreferrer" className={classes.a}>TheCocktialDB</a> API to to generate cocktail recipes through random search. <br/>Enjoy! 🍹🥂<br/>The full code and repository can also be viewed on my <a href="https://github.com/nhporti2/capstone_project/tree/main/cocktail-app" target="_blank" rel="noopener noreferrer" className={classes.a}>GitHub</a>.</p>
     </Container>
   );
 }
